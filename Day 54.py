61147f64f5089784771f4e528dfefd4c76f52e43
CelynChew <110886066+CelynChew@users.noreply.github.com>
2022-10-01 20:40:45 +0800

    Add files via upload

---
+++ Day 54.py
@@ -0,0 +1,14 @@
+# Aim: Finding Gradient Descent for Intercept
+
+def get_gradient_at_b(x, y, m, b):
+    diff = 0
+    N = len(x)
+    for i in range(0, len(x)):
+      y_val = y[i]
+      x_val = x[i]
+      diff += (y_val - ((m * x_val) + b))
+
+    b_gradient = -2/N * diff
+    return b_gradient
+
+print(get_gradient_at_b([1,2,3], [2,3,4], 3, 4))
