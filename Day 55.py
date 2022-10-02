8836e2d584bedc3aedbc8f97092fc4cc6f589c19
CelynChew <110886066+CelynChew@users.noreply.github.com>
2022-10-02 22:02:14 +0800

    Add files via upload

---
+++ Day 55.py
@@ -0,0 +1,11 @@
+# Aim: Finding Gradient Descent for Slope
+
+def get_gradient_at_m(x, y, m, b):
+    diff = 0
+    N = len(x)
+    for i in range(N):
+      y_val = y[i]
+      x_val = x[i]
+      diff += x_val*(y_val - ((m * x_val) + b))
+    m_gradient = -2/N * diff
+    return m_gradient
